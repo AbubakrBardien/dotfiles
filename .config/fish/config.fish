eb9209b6fb1df63b8c01e987956c0b0de09a312e
AbubakrBardien <abubakrbardien@gmail.com>
2024-07-17 04:12:10 +0200

    Added Fish shell configs and plugins.

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+set fish_greeting	# Supresses Fish's into message
+
+alias config "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME" 
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
