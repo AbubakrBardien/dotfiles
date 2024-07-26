a24d199f757d48d16c88b54e10be2878aa3ed79b
AbubakrBardien <abubakrbardien@gmail.com>
2024-07-26 20:11:45 +0200

    Added the 'lsd' and 'bat' commands and aliased them to 'ls' and 'cat' respectively

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 set fish_greeting	# Supresses Fish's into message
 
 alias config "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME" 
+alias ls "lsd"
+alias cat "bat"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
