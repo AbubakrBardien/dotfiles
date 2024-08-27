bb0d5da38e56a921f88180cf53709da8ced88ebf
AbubakrBardien <abubakrbardien@gmail.com>
2024-08-27 13:04:52 +0200

    Made some minor changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,16 @@ set fish_greeting	# Supresses Fish's into message
 alias config "/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME" 
 alias ls "lsd"
 alias cat "bat"
+alias tree "lsd --tree"
+
+set -x EDITOR "/usr/bin/nvim" # set editor environment variable
+
+# Fuction to be able to change directory when exiting. Call it with 'yy'
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
