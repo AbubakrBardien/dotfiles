9ae3a3428b2db95b9dd9e5bad51d2878b40b701a
AbubakrBardien <abubakrbardien@gmail.com>
2024-09-28 22:12:35 +0200

    The last Neovim save state before I start from scratch, in an attempt not to rely on NvChad

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,18 +2,26 @@ require "nvchad.options"
 
 -- add yours here!
 
- local opt = vim.opt
+local opt = vim.opt
 
- opt.cursorlineopt = 'both' -- to enable cursorline!
- opt.cursorcolumn = true
- opt.relativenumber = true
- opt.guicursor = ""
+opt.cursorlineopt = "both" -- to enable cursorline!
+opt.cursorcolumn = true
+opt.relativenumber = true
+opt.guicursor = ""
 
- vim.api.nvim_command("highlight CursorColumn guibg=#252931")
+vim.api.nvim_command "highlight CursorColumn guibg=#252931"
 
- opt.tabstop = 4
- opt.softtabstop = 4
- opt.shiftwidth = 4
- opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = false
 
- opt.scrolloff = 8
+opt.scrolloff = 8
+
+vim.cmd("set wildmenu")
+vim.cmd("set wildmode=longest:full,full")
+
+-- If no filename provided, open the dashboard
+if vim.fn.argc() == 0 then
+    vim.cmd("Nvdash")
+end
