7e4a7961677b6e4286f0062eec9d4ebdf1ced385
AbubakrBardien <abubakrbardien@gmail.com>
2024-09-28 21:46:02 +0200

    created a faster script (written in C) to count the number of packages, and use it as a module in fastfetch

---
+++ .scripts/count-packages/count_packages.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    FILE *fp1 = popen("pacman -Qen | wc -l", "r");
+    FILE *fp2 = popen("pacman -Qem | wc -l", "r");
+    FILE *fp3 = popen("pacman -Qdt | wc -l", "r");
+    
+    int numChars = 6; // allows to store up to 5 digits. The 6th is the null terminator character ('\0')
+
+    char pacman_pkgs[numChars];
+    char aur_pkgs[numChars];
+    char orphened_pkgs[numChars];
+
+    if (fgets(pacman_pkgs, sizeof(pacman_pkgs), fp1) != NULL &&
+        fgets(aur_pkgs, sizeof(aur_pkgs), fp2) != NULL &&
+        fgets(orphened_pkgs, sizeof(orphened_pkgs), fp3) != NULL) {
+        
+        // Remove trailing newline character
+        pacman_pkgs[strcspn(pacman_pkgs, "\n")] = 0;
+        aur_pkgs[strcspn(aur_pkgs, "\n")] = 0;
+        orphened_pkgs[strcspn(orphened_pkgs, "\n")] = 0;
+
+        printf("%s (Pacman), %s (AUR), %s (orphaned)", pacman_pkgs, aur_pkgs, orphened_pkgs);
+    }
+
+    pclose(fp1);
+    pclose(fp2);
+    pclose(fp3);
+
+    return 0;
+}
--- .scripts/count_packages.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# 'pacman -Qe' to see only programs explicitly installed, meaning no dependencies listed 
-
-pacman_pkgs=$(pacman -Qen | wc -l)
-aur_pkgs=$(pacman -Qem | wc -l)
-orphened_pkgs=$(pacman -Qdt | wc -l)
-
-echo "$pacman_pkgs (Pacman), $aur_pkgs (AUR), $orphened_pkgs (orphaned)"
