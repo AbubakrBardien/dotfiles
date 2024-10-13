90641a16a47af75cd5cc318ae1b3a10e8aadae71
AbubakrBardien <abubakrbardien@gmail.com>
2024-10-13 02:39:17 +0200

    Formatted 'count_packages.c'

--- .scripts/count-packages/count_packages.c
+++ .scripts/count-packages/count_packages.c
@@ -3,31 +3,33 @@
 #include <string.h>
 
 int main() {
-    FILE *fp1 = popen("pacman -Qen | wc -l", "r");
-    FILE *fp2 = popen("pacman -Qem | wc -l", "r");
-    FILE *fp3 = popen("pacman -Qdt | wc -l", "r");
-    
-    int numChars = 6; // allows to store up to 5 digits. The 6th is the null terminator character ('\0')
 
-    char pacman_pkgs[numChars];
-    char aur_pkgs[numChars];
-    char orphened_pkgs[numChars];
+	FILE *fp1 = popen("pacman -Qen | wc -l", "r");
+	FILE *fp2 = popen("pacman -Qem | wc -l", "r");
+	FILE *fp3 = popen("pacman -Qdt | wc -l", "r");
 
-    if (fgets(pacman_pkgs, sizeof(pacman_pkgs), fp1) != NULL &&
-        fgets(aur_pkgs, sizeof(aur_pkgs), fp2) != NULL &&
-        fgets(orphened_pkgs, sizeof(orphened_pkgs), fp3) != NULL) {
-        
-        // Remove trailing newline character
-        pacman_pkgs[strcspn(pacman_pkgs, "\n")] = 0;
-        aur_pkgs[strcspn(aur_pkgs, "\n")] = 0;
-        orphened_pkgs[strcspn(orphened_pkgs, "\n")] = 0;
+	int numChars = 6; // allows to store up to 5 digits.
+					  // The 6th is the null terminator character ('\0')
 
-        printf("%s (Pacman), %s (AUR), %s (orphaned)", pacman_pkgs, aur_pkgs, orphened_pkgs);
-    }
+	char pacman_pkgs[numChars];
+	char aur_pkgs[numChars];
+	char orphened_pkgs[numChars];
 
-    pclose(fp1);
-    pclose(fp2);
-    pclose(fp3);
+	if (fgets(pacman_pkgs, sizeof(pacman_pkgs), fp1) != NULL &&
+		fgets(aur_pkgs, sizeof(aur_pkgs), fp2) != NULL &&
+		fgets(orphened_pkgs, sizeof(orphened_pkgs), fp3) != NULL) {
 
-    return 0;
+		// Remove trailing newline character
+		pacman_pkgs[strcspn(pacman_pkgs, "\n")] = 0;
+		aur_pkgs[strcspn(aur_pkgs, "\n")] = 0;
+		orphened_pkgs[strcspn(orphened_pkgs, "\n")] = 0;
+
+		printf("%s (Pacman), %s (AUR), %s (orphaned)", pacman_pkgs, aur_pkgs, orphened_pkgs);
+	}
+
+	pclose(fp1);
+	pclose(fp2);
+	pclose(fp3);
+
+	return 0;
 }
