d2af7a883cd4f19e80240fbb8647c8960a2f6382
AbubakrBardien <abubakrbardien@gmail.com>
2024-10-16 17:22:22 +0200

    Added 2 aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,22 +5,20 @@ end
 set fish_greeting	# Supresses Fish's into message
 
 alias config "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME" 
-
 alias ls "lsd"
 alias cat "bat"
 alias tree "lsd --tree"
 alias pipes "pipes.sh"
 alias ping "gping"
-
 alias cp "cp -v"
 alias mv "mv -v"
 alias rm "rm -v"
-
 alias disk_usage "dust -r" # Disk Usage Viewer
 alias dir_size "du -sh"
 alias disks "duf --hide special --hide-mp /boot" # Display Disk Space
-
 alias benchmark "hyperfine" # Very useful benchmarking tool
+alias unset "set --erase" # unset environment variable
+alias delete_orphaned "sudo pacman -Rns \$(pacman -Qdtq)"
 
 # Set Environment Variables
 set -x EDITOR "/usr/bin/nvim"
