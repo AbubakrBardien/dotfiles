335d0c78fe855a7562f60621011a27ddae736f65
AbubakrBardien <abubakrbardien@gmail.com>
2024-10-28 21:35:35 +0200

    Finished writing my auto-install scripts

--- .local/share/my_scripts/setup_scripts/setup1.sh
+++ .local/share/my_scripts/setup_scripts/setup1.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#  ____                   ____            _                 
+# | __ )  __ _ ___  ___  / ___| _   _ ___| |_ ___ _ __ ___  
+# |  _ \ / _` / __|/ _ \ \___ \| | | / __| __/ _ \ '_ ` _ \ 
+# | |_) | (_| \__ \  __/  ___) | |_| \__ \ ||  __/ | | | | |
+# |____/ \__,_|___/\___| |____/ \__, |___/\__\___|_| |_| |_|
+#                               |___/                       
+
 loadkeys us # Loads US keyboard layout (default)
 
 echo "Enter name of disk to be partitioned: (default: /dev/sda)"
@@ -85,45 +92,83 @@ else
 fi
 
 # Install Essential Packages
-pacstrap -K /mnt base{,-devel} linux{,-firmware} grub efibootmgr $microcode_pkg vim git networkmanager bluez{,-utils}
+pacstrap -K /mnt base{,-devel} linux{,-firmware} grub efibootmgr $microcode_pkg vim git \
+networkmanager bluez{,-utils}
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Set Time-Zone
 if [ -z "$timeZone" ]; then
 	timeZone="Africa/Johannesburg"
 fi
 
 chosen_locale="en_US.UTF-8"
 
+# Set Time-Zone and Locale
+# The sed command uncomments the chosen Locale.
 arch-chroot /mnt <<-EOF1
 ln -sf "/usr/share/zoneinfo/$timeZone" /etc/localtime
 hwclock --systohc
 
 sed -i '/$chosen_locale UTF-8/s/^#\s*//g' /etc/locale.gen
 locale-gen
-awk "BEGIN { print \"LANG=$chosen_locale\" }" > /etc/locale.conf
-awk "BEGIN { print \"arch-linux\" }" > /etc/hostname
+echo "LANG=$chosen_locale" > /etc/locale.conf
+echo "arch-linux" > /etc/hostname
 
+exit
+EOF1
+
+# Set passwords, add user, and give user Sudo Permissions
+# The sed command uncomments the "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" line.
+arch-chroot /mnt <<-EOF1
 passwd <<-EOF2
 	$rootPass
 	$rootPass
 EOF2
 
 useradd -m -g users -G wheel,storage,power,video,audio -s /bin/bash "$userName"
-
 passwd "$userName" <<-EOF2
 	$userPass
 	$userPass
 EOF2
+sed -i '/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/s/^#\s*//g' /etc/sudoers
 
-sed -i '/%wheel ALL=(ALL:ALL) ALL/s/^#\s*//g' /etc/sudoers
+exit
+EOF1
 
+# Configure Grub, Enable 32 Bit Support
+# The sed commands uncomment the "[multilib]" line, and the line directly after that.
+arch-chroot /mnt <<-EOF1
 grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
+sed -i '/\[multilib\]/s/^#\s*//g' /etc/pacman.conf
+sed -i '/\[multilib\]/{n;s/^#\s*//;}' /etc/pacman.conf
+
+exit
+EOF1
+
+loginManager="sddm"
+
+# Install SDDM, Hyprland, nvidia drivers, and terminal emulators. (kitty terminal is for temporary use)
+# Configure SDDM to luanch Hyprland
+# The sed commands do the following:
+# - Matches a line starting with "DisplayServer=", and sets it equal to "x11-user"
+# - Matches a line starting with "CompositorCommand=", and sets it equal to "Hyprland"
+arch-chroot /mnt <<-EOF1
+pacman -S --noconfirm $loginManager nvidia{,-utils} lib32-nvidia-utils hyprland kitty foot
+
+mkdir /etc/sddm.conf.d/
+cp /usr/lib/sddm/sddm.conf.d/default.conf /etc/sddm.conf.d/sddm.conf
+sed -i 's/^DisplayServer=.*/DisplayServer=x11-user/' /etc/sddm.conf.d/sddm.conf
+sed -i 's/^CompositorCommand=.*/CompositorCommand=Hyprland/' /etc/sddm.conf.d/sddm.conf
+
+exit
+EOF1
+
+arch-chroot /mnt <<-EOF1
 systemctl enable NetworkManager
 systemctl enable bluetooth
+systemctl enable $loginManager
 exit
 EOF1
 
