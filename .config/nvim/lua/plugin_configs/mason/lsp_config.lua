a1edd3ecb61b7485111a134c644b719f24345922
AbubakrBardien <abubakrbardien@gmail.com>
2024-11-06 11:52:10 +0200

    Added CSS Language Server to 'lsp_set'

--- .config/nvim/lua/plugin_configs/mason/lsp_config.lua
+++ .config/nvim/lua/plugin_configs/mason/lsp_config.lua
@@ -21,7 +21,7 @@ return {
 			-- local attachments =
 
 			-- this is where you configure each LSP
-			local lsp_set = { "lua_ls", "clangd", "pyright", "bashls" }
+			local lsp_set = { "lua_ls", "clangd", "pyright", "bashls", "cssls" }
 			for _, lsp in pairs(lsp_set) do
 				lspconfig[lsp].setup { capabilities = capabilities }
 			end
