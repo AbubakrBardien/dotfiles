7c90399500811d6a3d15faca169414973157739f
AbubakrBardien <abubakrbardien@gmail.com>
2024-11-08 21:36:13 +0200

    minor changes

--- .config/nvim/lua/plugin_configs/mason/lsp_config.lua
+++ .config/nvim/lua/plugin_configs/mason/lsp_config.lua
@@ -18,8 +18,6 @@ return {
 			-- allows LSPs to communicate with "cmp_nvim_lsp" to provide language-specific completions
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- local attachments =
-
 			-- this is where you configure each LSP
 			local lsp_set = { "lua_ls", "clangd", "pyright", "bashls", "cssls" }
 			for _, lsp in pairs(lsp_set) do
