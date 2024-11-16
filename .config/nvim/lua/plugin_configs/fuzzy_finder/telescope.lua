f9d5f9928f99c3dc84d70671e0b6c3fba7d88159
AbubakrBardien <abubakrbardien@gmail.com>
2024-11-17 00:12:39 +0200

    Fixed some unnecessary indenting

--- .config/nvim/lua/plugin_configs/fuzzy_finder/telescope.lua
+++ .config/nvim/lua/plugin_configs/fuzzy_finder/telescope.lua
@@ -1,33 +1,31 @@
 local common_deps = require("dependency_list")
 
 return {
-	{
-		-- Search Tool / Fuzzy Finder
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			common_deps["nvim_web_devicons"],
-		},
-		config = function()
-			require("telescope").setup {
-				defaults = {
-					sorting_strategy = "ascending",
-					layout_config = {
-						prompt_position = "top",
-						height = 0.68,
-						width = 0.85,
-					},
-				},
-				pickers = {
-					keymaps = {
-						layout_config = {
-							height = 24,
-							width = 95,
-						},
-					},
-				},
-			}
-		end,
+	-- Search Tool / Fuzzy Finder
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		common_deps["nvim_web_devicons"],
 	},
+	config = function()
+		require("telescope").setup {
+			defaults = {
+				sorting_strategy = "ascending",
+				layout_config = {
+					prompt_position = "top",
+					height = 0.68,
+					width = 0.85,
+				},
+			},
+			pickers = {
+				keymaps = {
+					layout_config = {
+						height = 24,
+						width = 95,
+					},
+				},
+			},
+		}
+	end,
 }
