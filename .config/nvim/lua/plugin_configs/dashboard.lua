d15879795ba998473e6b22939a3f0469202e7a44
AbubakrBardien <abubakrbardien@gmail.com>
2024-11-16 23:56:41 +0200

    Updated Neovim dashboard

--- .config/nvim/lua/plugin_configs/dashboard.lua
+++ .config/nvim/lua/plugin_configs/dashboard.lua
@@ -56,11 +56,11 @@ return {
 		config = {
 			header = art2,
 			center = {
-				{ icon = " ", desc = "Lazy Menu", action = ":Lazy" },
-				{ icon = " ", desc = "Mason Menu", action = ":Mason" },
+				{ icon = " ", desc = "Lazy Menu", action = "Lazy" },
+				{ icon = " ", desc = "Mason Menu", action = "Mason" },
+				{ icon = " ", desc = "Nvim Configs", action = "Telescope find_files cwd=$HOME/.config/nvim" },
 				{ icon = " ", desc = "Find Files", key = "Space ff", key_format = shortcut_fmt, action = "Telescope find_files" },
 				{ icon = "󰚰 ", desc = "Recent Files", key = "Space fr", key_format = shortcut_fmt, action = "Telescope oldfiles" },
-				{ icon = " ", desc = "Manual Pages", key = "Space fh", key_format = shortcut_fmt, action = "Telescope help_tags" }
 			},
 		},
 		shortcut_type = "number"
