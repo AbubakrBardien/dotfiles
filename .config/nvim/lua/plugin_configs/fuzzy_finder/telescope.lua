9116ca9f2dec1e4fb026b1adf7efcc4afefc07c9
AbubakrBardien <abubakrbardien@gmail.com>
2024-11-21 15:52:56 +0200

    Changed the layout of Telescope "current_buffer_fuzzy_find" picker

--- .config/nvim/lua/plugin_configs/fuzzy_finder/telescope.lua
+++ .config/nvim/lua/plugin_configs/fuzzy_finder/telescope.lua
@@ -1,5 +1,8 @@
 local common_deps = require("dependency_list")
 
+local size_keymaps = { height = 24, width = 95 }
+local size_fuzzy_find = { height = 0.65, width = 0.65 }
+
 return {
 	-- Search Tool / Fuzzy Finder
 	"nvim-telescope/telescope.nvim",
@@ -8,6 +11,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		common_deps["nvim_web_devicons"],
 	},
+
 	config = function()
 		require("telescope").setup {
 			defaults = {
@@ -20,10 +24,11 @@ return {
 			},
 			pickers = {
 				keymaps = {
-					layout_config = {
-						height = 24,
-						width = 95,
-					},
+					layout_config = size_keymaps,
+				},
+				current_buffer_fuzzy_find = {
+					previewer = false,
+					layout_config = size_fuzzy_find,
 				},
 			},
 		}
