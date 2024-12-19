4da883f14abb4311298c0d8b3cc4b3eba5328926
AbubakrBardien <abubakrbardien@gmail.com>
2024-12-19 16:07:23 +0200

    Changed the Shell icon (from Mini.Icons) to Green
    	modified:   .config/nvim/lua/plugin_configs/mini_nvim/icons.lua
    	modified:   .config/nvim/lua/highlights.lua

--- .config/nvim/lua/plugin_configs/mini_nvim/icons.lua
+++ .config/nvim/lua/plugin_configs/mini_nvim/icons.lua
@@ -8,6 +8,9 @@ return {
 				py = { hl = "PythonIconColor" }, -- custom highlight group
 				json = { glyph = "" },
 			},
+			filetype = {
+				sh = { hl = "ShellIconColor" },
+			},
 		}
 		MiniIcons.mock_nvim_web_devicons()
 	end,
