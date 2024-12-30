dbe6ecc8ea5efc923e34a1ffe874bf9c6868f1a8
AbubakrBardien <abubakrbardien@gmail.com>
2024-12-31 01:05:47 +0200

    Change Python Icon Color in Mini.icons

--- .config/nvim/lua/highlights.lua
+++ .config/nvim/lua/highlights.lua
@@ -22,7 +22,7 @@ vim.api.nvim_command("highlight NvimDapVirtualText guifg=#4f4e5c")
 vim.api.nvim_command("highlight NvimDapVirtualTextChanged guifg=#616073 gui=bold")
 
 -- Mini Icons
-vim.api.nvim_command("highlight PythonIconColor guifg=#71B0E8")
+vim.api.nvim_command("highlight PythonIconColor guifg=#5ba0de")
 vim.api.nvim_command("highlight ShellIconColor guifg=#96d16b")
 
 -- Code Folding
