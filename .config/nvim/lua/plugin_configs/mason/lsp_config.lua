f80c90943a5ae475980685877e0ef75c6310bd6b
AbubakrBardien <abubakrbardien@gmail.com>
2025-02-16 02:11:27 +0200

    Re-orginized the 'lsp_config.lua' file

--- .config/nvim/lua/plugin_configs/mason/lsp_config.lua
+++ .config/nvim/lua/plugin_configs/mason/lsp_config.lua
@@ -1,14 +1,9 @@
 ------------------------- LSP Setup and Auto-Installations -------------------------
+
+-- local lsp_set = { "lua_ls", "ruff", "bashls" }
+local lsp_set = { "lua_ls", "clangd", "pyright", "bashls", "cssls" }
+
 return {
-	{
-		-- Tool to bridge the gap between "mason.nvim" and "lspconfig"
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup {
-				automatic_installation = true, -- Auto-install LSPs if it's configured in "nvim-lspconfig"
-			}
-		end,
-	},
 	{
 		"folke/lazydev.nvim",
 		ft = "lua", -- only load on lua files
@@ -21,6 +16,15 @@ return {
 		},
 	},
 	{
+		-- Tool to bridge the gap between "mason.nvim" and "lspconfig"
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup {
+				ensure_installed = lsp_set,
+			}
+		end,
+	},
+	{
 		-- Allows Neovim to communicate with Language Servers
 		"neovim/nvim-lspconfig",
 		dependencies = {
@@ -29,17 +33,15 @@ return {
 		},
 		config = function()
 			local lspconfig = require("lspconfig")
-
-			-- Doing the same configuration for multiple LSPs
-			-- local lsp_set = { "lua_ls", "ruff", "bashls" }
-			local lsp_set = { "lua_ls", "pyright", "bashls" }
 			for _, lsp in pairs(lsp_set) do
-				lspconfig[lsp].setup {}
+				if lsp == "clangd" then
+					lspconfig[lsp].setup { -- "clang-tidy" is the built-in linter
+						cmd = { "clangd", "--offset-encoding=utf-16", "--clang-tidy" },
+					}
+				else
+					lspconfig[lsp].setup {}
+				end
 			end
-
-			lspconfig["clangd"].setup { -- "clang-tidy" is the built-in linter
-				cmd = { "clangd", "--offset-encoding=utf-16", "--clang-tidy" },
-			}
 		end,
 	},
 	-- Check the docs in both repos to verify if a language server can be auto-installed and configured
