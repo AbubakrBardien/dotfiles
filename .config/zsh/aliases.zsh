f685e364eb17a019fc34f9763fdc61d52898afe1
AbubakrBardien <abubakrbardien@gmail.com>
2025-03-14 18:03:57 +0200

    Minor Changes

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -14,18 +14,18 @@ alias add="config add"
 alias commit="config commit"
 alias push="config push"
 
-alias cat="bat"
-alias pipes="pipes.sh"
-alias ping="gping"
-
 alias cp="cp -v"
 alias mv="mv -v"
 alias rm="rm -v"
+
+alias fk="sudo !!"
 
 alias disks="duf /" # Display Disk Space
 alias dir_size="du -sh" # Directory Size
 alias disk_usage="dust -r" # Disk Usage Viewer
 
 alias benchmark="hyperfine" # Very useful benchmarking tool
-
-alias fk="sudo !!"
+alias cat="bat"
+alias pipes="pipes.sh"
+alias ping="gping"
+alias fetch="fastfetch"
