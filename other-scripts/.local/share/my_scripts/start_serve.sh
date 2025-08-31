c79f7a272440dbad0bc6b54b5285b4537282a29a
AbubakrBardien <abubakrbardien@gmail.com>
2025-08-31 15:34:04 +0200

    Refining a script

--- other-scripts/.local/share/my_scripts/start_serve.sh
+++ other-scripts/.local/share/my_scripts/start_serve.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# shellcheck disable=SC2086
+# Set XDG_STATE_HOME if it's not already set
+XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
 
 startpage_dir=$HOME/Documents/External_Repos/browser-startpage/_site
 
 # Check if the directory exists
-if [ ! -d $XDG_STATE_HOME/serve ]; then
-	mkdir $XDG_STATE_HOME/serve
+if [ ! -d "$XDG_STATE_HOME/serve" ]; then
+	mkdir "$XDG_STATE_HOME/serve"
 fi
 
 errorFile=$XDG_STATE_HOME/serve/serve.err
 
 # Serve the startpage in the background
-serve $startpage_dir 1>/dev/null 2>$errorFile &
+serve "$startpage_dir" 1>/dev/null 2>"$errorFile" &
