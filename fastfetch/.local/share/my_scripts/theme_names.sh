c881a3d3933a80b4c2268e2c3f89f9fdb2b229ec
AbubakrBardien <abubakrbardien@gmail.com>
2025-09-07 20:17:46 +0200

    Added a script to display themes info in Fastfetch the way I want to display it

---
+++ fastfetch/.local/share/my_scripts/theme_names.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2164
+cd "$XDG_CONFIG_HOME"
+
+# Store the names of the gtk folders in an array
+gtkVersionsFolderNames=(gtk*)
+
+# Decide on what info to display
+case "$1" in
+0)
+	gtk_theme=$(grep -Po '^gtk-theme-name="?\K(.+?)(?= \d+$|$)' "$XDG_CONFIG_HOME/${gtkVersionsFolderNames[0]}/settings.ini" 2>/dev/null)
+	qt_theme=$(grep -Po '^theme=\K.+(?=#$)' "$XDG_CONFIG_HOME/Kvantum/kvantum.kvconfig")
+	if [ -n "$qt_theme" ]; then
+		ouputStr="${gtk_theme} [GTK], ${qt_theme} [Qt]"
+	else
+		ouputStr="${gtk_theme} [GTK]"
+	fi
+	;;
+1)
+	gtk_icon=$(grep -Po '^gtk-icon-theme-name="?\K(.+?)(?= \d+$|$)' "$XDG_CONFIG_HOME/${gtkVersionsFolderNames[0]}/settings.ini" 2>/dev/null)
+	ouputStr="${gtk_icon} [GTK]"
+	;;
+2)
+	gtk_font=$(grep -Po '^gtk-font-name="?\K(.+?)(?= \d+$|$)' "$XDG_CONFIG_HOME/${gtkVersionsFolderNames[0]}/settings.ini" 2>/dev/null)
+	ouputStr="${gtk_font} [GTK]"
+	;;
+esac
+
+echo "$ouputStr"
