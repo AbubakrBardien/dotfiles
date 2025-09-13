27d0bcf7df3f63bd5c755e528aad8d96ba13de38
AbubakrBardien <abubakrbardien@gmail.com>
2025-09-14 01:22:06 +0200

    Made Bash scripts more reliant by making sure it works even when the 'XDG Base Directory' Environment Variables aren't set

--- fastfetch/.local/share/my_scripts/theme_names.sh
+++ fastfetch/.local/share/my_scripts/theme_names.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC2164
-cd "$XDG_CONFIG_HOME"
+cd "${XDG_CONFIG_HOME:-$HOME/.config}"
 
 # Store the names of the gtk folders in an array
 gtkVersionsFolderNames=(gtk*)
@@ -9,8 +9,8 @@ gtkVersionsFolderNames=(gtk*)
 # Decide on what info to display
 case "$1" in
 0)
-	gtk_theme=$(grep -Po '^gtk-theme-name="?\K(.+?)(?= \d+$|$)' "$XDG_CONFIG_HOME/${gtkVersionsFolderNames[0]}/settings.ini" 2>/dev/null)
-	qt_theme=$(grep -Po '^theme=\K.+(?=#$)' "$XDG_CONFIG_HOME/Kvantum/kvantum.kvconfig")
+	gtk_theme=$(grep -Po '^gtk-theme-name="?\K(.+?)(?= \d+$|$)' "${XDG_CONFIG_HOME:-$HOME/.config}/${gtkVersionsFolderNames[0]}/settings.ini" 2>/dev/null)
+	qt_theme=$(grep -Po '^theme=\K.+(?=#$)' "${XDG_CONFIG_HOME:-$HOME/.config}/Kvantum/kvantum.kvconfig")
 	if [ -n "$qt_theme" ]; then
 		ouputStr="${gtk_theme} [GTK], ${qt_theme} [Qt]"
 	else
@@ -18,11 +18,11 @@ case "$1" in
 	fi
 	;;
 1)
-	gtk_icon=$(grep -Po '^gtk-icon-theme-name="?\K(.+?)(?= \d+$|$)' "$XDG_CONFIG_HOME/${gtkVersionsFolderNames[0]}/settings.ini" 2>/dev/null)
+	gtk_icon=$(grep -Po '^gtk-icon-theme-name="?\K(.+?)(?= \d+$|$)' "${XDG_CONFIG_HOME:-$HOME/.config}/${gtkVersionsFolderNames[0]}/settings.ini" 2>/dev/null)
 	ouputStr="${gtk_icon} [GTK]"
 	;;
 2)
-	gtk_font=$(grep -Po '^gtk-font-name="?\K(.+?)(?= \d+$|$)' "$XDG_CONFIG_HOME/${gtkVersionsFolderNames[0]}/settings.ini" 2>/dev/null)
+	gtk_font=$(grep -Po '^gtk-font-name="?\K(.+?)(?= \d+$|$)' "${XDG_CONFIG_HOME:-$HOME/.config}/${gtkVersionsFolderNames[0]}/settings.ini" 2>/dev/null)
 	ouputStr="${gtk_font} [GTK]"
 	;;
 esac
--- other-scripts/.local/share/my_scripts/start_serve.sh
+++ other-scripts/.local/share/my_scripts/start_serve.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-# Set XDG_STATE_HOME if it's not already set
-XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
-
 startpage_dir=$HOME/Documents/External_Repos/browser-startpage/_site
 
 # Check if the directory exists
-if [ ! -d "$XDG_STATE_HOME/serve" ]; then
-	mkdir "$XDG_STATE_HOME/serve"
+if [ ! -d "${XDG_STATE_HOME:-$HOME/.local/state}/serve" ]; then
+	mkdir "${XDG_STATE_HOME:-$HOME/.local/state}/serve"
 fi
 
-errorFile=$XDG_STATE_HOME/serve/serve.err
+errorFile=${XDG_STATE_HOME:-$HOME/.local/state}/serve/serve.err
 
 # Serve the startpage in the background
 serve "$startpage_dir" 1>/dev/null 2>"$errorFile" &
